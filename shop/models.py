a1e602fe1a0de1afcd6c35af616cf2f4f22fcd58
Nikita <nikigros48@gmail.com>
2026-05-31 01:55:59 +0700

    Task 5

---
+++ shop/models.py
@@ -0,0 +1,30 @@
+import uuid
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    attributes = models.ManyToManyField('Attribute')
+
+
+class ProductImage(models.Model):
+    image = models.ImageField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
