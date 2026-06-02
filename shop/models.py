6d757baf0ec719823b3b327d9d1baf8281d69e25
Nikita <nikigros48@gmail.com>
2026-06-03 02:48:30 +0700

    Create ProductManager

--- shop/models.py
+++ shop/models.py
@@ -2,12 +2,29 @@ import uuid
 from django.db import models
 
 
+class ProductManager(models.Manager):
+
+
+    def in_stock(self):
+        return self.filter(stock__gt=0)
+    
+
+    def items_by_descending_price(self):
+        return self.order_by('-price')
+    
+
+    def items_by_ascending_price(self):
+        return self.order_by('price')
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
     stock = models.IntegerField()
     attributes = models.ManyToManyField('Attribute')
+
+    objects = ProductManager()
 
 
 class ProductImage(models.Model):
