951adce7092a3d60d8c6a8491dbb4a5c1a960cb8
Nikita <nikigros48@gmail.com>
2026-06-18 14:59:51 +0700

    homework admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from shop.models import Product
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'stock')
+
