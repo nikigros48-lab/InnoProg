71cf2fe198dc607fafa39ec46392822f9e7b7be6
Nikita <nikigros48@gmail.com>
2026-06-18 23:06:37 +0700

    homework views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from .models import Product
 
 # Create your views here.
+def all_products(request: HttpRequest) -> HttpResponse:
+    products = Product.objects.all()
+    return HttpResponse(f"<p>{product}</p>" for product in products)
